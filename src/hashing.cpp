c3b0bc81551ef536bc240b6f07d3360e3b78029a
theczechr <84510533+theczechr@users.noreply.github.com>
2022-05-12 13:38:37 +0200

    Added comments to hashing.cpp
    Adding additional information about hashing functions.

--- src/hashing.cpp
+++ src/hashing.cpp
@@ -1,18 +1,16 @@
 #include <functional>
 #include <math.h>
 #include "hashing.h"
-size_t hash(std::string to_hash)
+size_t hash(std::string to_hash) // advanced hashing func from std::hash, used for hashing everything except client ids.
 {
     std::size_t str_hash = std::hash<std::string>{}(to_hash);
     return str_hash;
 }
 
-size_t client_id(std::string email) {
+size_t client_id(std::string email) // basic hashing func, used for making client ids. 
+{
     size_t user_id = 0;
     int number = 5;
-    //need to do some bitwise calculations
-    // nebudu delat zadne calculations zde jiz na to jebu, bro ... :D =]
-    // neni na to sila:D =[
     for (size_t i = 0; i < email.length(); i++) {
         user_id = user_id + (pow(number, i) * sqrt(size_t(email[i])) );
     }
