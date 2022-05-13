a5b5ea892a66a2bf07fae78f820b9802d7ff05b8
karl0st <k.raffay@protonmail.com>
2022-05-13 21:35:21 +0200

    syntax

--- src/console.cpp
+++ src/console.cpp
@@ -1,5 +1,6 @@
 #include "console.hpp"
 
-void flush() { // clearing out the console
+void flush() // clearing out the console
+{
 	std::system("CLS"); // this works only for windows
 }
--- src/hashing.cpp
+++ src/hashing.cpp
@@ -1,11 +1,13 @@
 #include "hashing.hpp"
 
-size_t hash(std::string to_hash) { // advanced hashing func from std::hash, used for hashing everything except client ids
+size_t hash(std::string to_hash) // advanced hashing func from std::hash, used for hashing everything except client ids
+{
     std::size_t str_hash = std::hash<std::string>{}(to_hash);
     return str_hash;
 }
 
-size_t client_id(std::string email) { // basic hashing func, used for making client ids
+size_t client_id(std::string email) // basic hashing func, used for making client ids
+{
     size_t user_id = 0;
     int number = 5;
     for (size_t i = 0; i < email.length(); i++) {
