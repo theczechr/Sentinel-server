999f3cf303cc99c0a427c3d53122f7aedb0a3188
theczechr <robinjarusek@gmail.com>
2022-05-15 22:11:37 +0200

    Changed Account variables to public

--- account/account.hpp
+++ account/account.hpp
@@ -8,11 +8,6 @@
 class Account
 {
 private:
-	size_t username;
-	size_t email;
-	size_t password;
-	int phone_number;
-
 	bool valid_username(std::string username);
 	bool valid_email(std::string email);
 	bool valid_password(std::string password);
@@ -22,13 +17,16 @@ private:
 	void change_email();
 	void change_password();
 public:
+	size_t hashed_username;
+	size_t username;
+	size_t email;
+	size_t password;
+	int phone_number;
 	void create(std::string username, std::string email, std::string password, int phone_number);
 	void login(std::string username, std::string email, std::string password, int phone_number);
-
 	void close(); // Delete account
 	void reopen(); // Only within x days ?
 };
-
 // Mozna zvlast funkce na registraci a v te funkci by se proste vytvarel Account
 // Musime se o tom pobavit, ale podle me to bude lepsi
 // Premyslel jsem nad tim vic a ztratil jsem se v tom
