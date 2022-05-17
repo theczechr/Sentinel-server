61a226e96fa9620f326fcca426e96e576b2b6af2
karl0st <k.raffay@protonmail.com>
2022-05-17 18:21:23 +0200

    renamed variables, added sqlite3 to CMake, added vcpkg command for sqlite3 installation to README.md

--- account/account.hpp
+++ account/account.hpp
@@ -11,12 +11,8 @@ public:
 	size_t hashed_phone_number;
 
 	[[nodiscard]] bool check_valid(const std::string& to_be_checked, size_t string_from, size_t string_to, bool allow_special_char) const;
-	void create(const std::string& username, const std::string& email, const std::string& password, int phone_number);
-	void login(const std::string& username, std::string email, std::string password, int phone_number) const;
+	void create(const std::string& username, const std::string& email, const std::string& password, const std::string& phone_number);
+	void login(const std::string& username, std::string email, std::string password, const std::string& phone_number) const;
 	void close(); // Delete account
 	void reopen(); // Only within x days ?
 };
-
-// Mozna zvlast funkce na registraci a v te funkci by se proste vytvarel Account
-// Musime se o tom pobavit, ale podle me to bude lepsi
-// Premyslel jsem nad tim vic a ztratil jsem se v tom
